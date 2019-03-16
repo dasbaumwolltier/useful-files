0ce187448d920d94bd6984f8fff9da6cc8011ced
dasbaumwolltier <gabriel@guldner.eu>
2019-03-16 19:09:16 +0100

    added for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,14 @@
 export SSH_ASKPASS=(which ksshaskpass)
+export GPG_TTY=(tty)
+
+pgrep gpg-agent
+if $status == 1
+    gpg-agent --daemon --enable-ssh-support
+end
+export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 
 function start --description "Open using the default program"
-    xdg-open $argv[1..-1]   
+    xdg-open $argv[1..-1]
 end
 
 function ls --description "ls with color"
