982ccdffa9066e85cf583c054ac6f88e524953ff
dasbaumwolltier <gabriel@guldner.eu>
2019-03-28 00:45:24 +0100

    updated stuff

--- fish/conf.d/freebsd-ccache.fish
+++ fish/conf.d/freebsd-ccache.fish
@@ -1,10 +1,10 @@
 set ccacheSize = "100G"
 
 # set ccache varibles
-setenv PATH /usr/local/libexec/ccache:$PATH
-setenv CCACHE_PATH /usr/bin:/usr/local/bin
-setenv CCACHE_DIR /var/tmp/ccache
-setenv CCACHE_LOGFILE /var/log/ccache.log
+setenv PATH "/usr/local/libexec/ccache:$PATH"
+setenv CCACHE_PATH "/usr/bin:/usr/local/bin"
+setenv CCACHE_DIR "/var/tmp/ccache"
+setenv CCACHE_LOGFILE "/var/log/ccache.log"
 
 # set ccache temp size to 512MB (default 1GB)
 if test -x /usr/local/bin/ccache
--- fish/conf.d/yubikey.fish
+++ fish/conf.d/yubikey.fish
@@ -1,14 +1,14 @@
 export SSH_ASKPASS=(which ksshaskpass)
 export GPG_TTY=(tty)
 
-pgrep ssh-agent
-if $status == 0
+pgrep ssh-agent 2>&1 > /dev/null
+if test $status -eq 0
     killall ssh-agent 2>&1 > /dev/null
-    killall gpg-agent 2>& 1 >/dev/null
+    killall gpg-agent 2>&1 >/dev/null
 end
 
-pgrep gpg-agent
-if $status == 1
-    gpg-agent --daemon --enable-ssh-support
+pgrep gpg-agent 2>&1 > /dev/null
+if test $status -eq 1
+    gpg-agent --daemon --enable-ssh-support 2>&1 > /dev/null
 end
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
