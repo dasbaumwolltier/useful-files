6b1039797989cbc74c2bbf19cd810bece5312c29
dasbaumwolltier <gabriel@guldner.eu>
2019-03-27 18:23:25 +0100

    added ccache

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set filename (status --current-filename)
-set currentUserDir ~(whoami)
+set currentUserDir "~(whoami)"
 
-if not (string match -q -- "$currentUserDir*" $filename)
+if not string match -q -- "$currentUserDir*" $filename
     echo -e "\033[1;33mYou are not running the config.fish as the user who it belongs to!\033[0m"
 end
 
@@ -13,7 +13,7 @@ function ls --description "ls with color"
     /bin/ls --color=always $argv[1..-1]
 end
 
-if (test -d conf.d)
+if test -d conf.d
     for configFile in conf.d/*
         source $configFile
     end
