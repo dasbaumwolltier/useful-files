603121bbc72e1b95fe2c06fa49dbfc9b70597eb8
dasbaumwolltier <gabriel@guldner.eu>
2019-03-27 15:22:59 +0100

    added possibility to use conf.d to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+set filename (status --current-filename)
+set currentUserDir ~(whoami)
+
+if not (string match -q -- "$currentUserDir*" $filename)
+    echo -e "\033[1;33mYou are not running the config.fish as the user who it belongs to!\033[0m"
+end
+
 export SSH_ASKPASS=(which ksshaskpass)
 export GPG_TTY=(tty)
 
@@ -13,4 +20,10 @@ end
 
 function ls --description "ls with color"
     /bin/ls --color=always $argv[1..-1]
+end
+
+if (test -d conf.d)
+    for configFile in conf.d/*
+        source $configFile
+    end
 end
