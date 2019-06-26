8498b376a65a6ffcb0c395a857ba56a97dd013d4
dasbaumwolltier <gabriel@guldner.eu>
2019-06-26 16:00:01 +0200

    added pynodered

---
+++ server/pynodered/pynodered.py
@@ -0,0 +1,37 @@
+import email
+import imaplib
+
+from matrix_client import client, api
+from pynodered import node_red, NodeProperty
+
+@node_red(category='pyfuncts')
+def get_all_emails_body_unique_and_send_over_matrix_daemons(node, msg):
+    connection = imaplib.IMAP4('mail.guldner.eu')
+
+    connection.starttls()
+    connection.login('daemons@guldner.eu', 'ebAnbQAANGcAn3GWXHE9vntUqSBmVt2gtwuV2AvcVm6MFrQc5L3CwHua8rJTzyLJQGbjTP5uDZkvB4kHq3EGkqZJ3RRtAXgCUKSt')
+
+    connection.select('INBOX')
+    retcode, messages = connection.search(None, '(UNSEEN)')
+
+    filteredMessages = list()
+
+    if retcode == 'OK':
+        for messageNum in messages[0].split(b' '):
+            print('Fetching message number: ', messageNum)
+
+            _, data = connection.fetch(messageNum, '(RFC822)')
+            msg: email.message.Message = email.message_from_bytes(data[0][1])
+            msgStr = msg.get_payload()
+
+            if msgStr not in filteredMessages:
+                filteredMessages.append(msgStr)
+
+    if len(filteredMessages) == 0:
+        return msg
+
+    cli = client.MatrixClient('https://chat.guldner.eu', token='MDAxOGxvY2F0aW9uIGd1bGRuZXIuZXUKMDAxM2lkZW50aWZpZXIga2V5CjAwMTBjaWQgZ2VuID0gMQowMDIzY2lkIHVzZXJfaWQgPSBAbnJlZDpndWxkbmVyLmV1CjAwMTZjaWQgdHlwZSA9IGFjY2VzcwowMDIxY2lkIG5vbmNlID0gRU46LUoxKjdxKmNVfk07SAowMDJmc2lnbmF0dXJlIJ23P_JFtJ1rCFL3jUP6LOnfph0z6_t9uBPsjxmXWxwJCg', user_id='@nred:guldner.eu')
+    room: client.Room = cli.join_room('!SmSdHbmEVBqDlcSbug:guldner.eu')
+
+    for message in filteredMessages:
+        room.send_text(message)
